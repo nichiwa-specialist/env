4c1516119d644a46dbcac8813dbf49e355f9845b
Y Okumura <y-okumura@nichiwa-system.co.jp>
2016-01-31 18:51:44 +0900

    まずはAndroid開発に必要なものを一通りリストアップ。

---
+++ install.bat
@@ -0,0 +1,5 @@
+cd %~dp0
+
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+cinst -y packages.config
